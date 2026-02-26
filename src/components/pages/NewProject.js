0b4fb9ff126d01f8c5a26815db9aa7b3b9bc505d
euler.amorim <euler.amorim@hotmail.com>
2026-02-26 14:59:49 -0300

    project form page

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,12 @@
+import ProjectForm from "../project/ProjectForm";
+import style from "./NewProject.module.css";
+
 function NewProject() {
   return (
-    <div>
-      <h1>New Project</h1>
+    <div className={style.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
     </div>
   );
 }
