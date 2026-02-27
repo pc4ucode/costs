52b7b12cb2310bcf08fd71055b961c75841b5cc4
euler.amorim <euler.amorim@hotmail.com>
2026-02-27 11:00:06 -0300

    Implement message component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,20 @@
+import { useLocation } from "react-router-dom";
+import Message from "../layout/Message";
+
 function Projects() {
-  return <p>Projects</p>;
+  const location = useLocation();
+  let message = "";
+
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message type="success" msg={message} />}
+    </div>
+  );
 }
 
 export default Projects;
